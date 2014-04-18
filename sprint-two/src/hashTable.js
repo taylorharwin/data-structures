365fc2df6e8322de3250d52cda324cf7feb8b351
Taylor Harwin <taylor.harwin@gmail.com>
2014-04-18 14:11:28 -0700

    Completed Hash Table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,12 +5,25 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var toAdd = [k, v]
+  this._storage.set(i, toAdd);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  if (this._storage.get(i) === null){
+  	return null
+  }
+  var ans = this._storage.get(i);
+  return ans[1];
+  
 };
 
 HashTable.prototype.remove = function(k){
+	var i = getIndexBelowMaxForKey(k, this._limit);
+	  return this._storage.set(i, null);
 };
+
+	
+
 
