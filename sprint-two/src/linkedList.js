e0c122f8ac3ad9e7517ce4bada668fd0c81f27c9
Taylor Harwin <taylor.harwin@gmail.com>
2014-04-18 14:25:01 -0700

    Adding to tail should add to tail

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,15 +2,42 @@ var makeLinkedList = function(){
   var list = {};
   list.head = null;
   list.tail = null;
+  list.makeNode = makeNode;
 
-  list.addToTail = function(value){
+   list.addToTail = function(value){
+    var newNode = makeNode(value);
+    if(list.head === null) {
+      list.head = newNode;
+      list.tail = newNode;
+    }
+    else if(list.head){
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
+
   };
 
   list.removeHead = function(){
+    list.head = list.head.next;
+
+    return list.head.value;
+
+  
   };
 
   list.contains = function(target, node){
-  };
+    node = node || list.head;
+    
+    if (node.value === target){
+        return true;
+      }
+
+    if (node.next === null){
+      return false;
+    }
+      return list.contains(target, node.next);
+    };
+    
 
   return list;
 };
@@ -21,4 +48,4 @@ var makeNode = function(value){
   node.next = null;
 
   return node;
-};
+};  
